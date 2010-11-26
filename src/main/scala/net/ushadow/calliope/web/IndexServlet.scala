8a0a4387033b9f01886db064aaca4377e2c7e391
alexei guevara <alexguev@gmail.com>
2010-11-26 00:00:00 -0500

    rum rum

---
+++ src/main/scala/net/ushadow/calliope/web/IndexServlet.scala
@@ -0,0 +1,12 @@
+package net.alexguev.tweetadorn.web
+
+import javax.servlet.http._
+
+class IndexServlet extends HttpServlet {
+
+  override def doGet(req: HttpServletRequest, resp: HttpServletResponse) =
+    resp.getWriter().print("<HTML>" +
+      "<HEAD><TITLE>Hello, Scala!</TITLE></HEAD>" +
+      "<BODY>Hello, Scala! This is a servlet.</BODY>" +
+      "</HTML>")
+}
