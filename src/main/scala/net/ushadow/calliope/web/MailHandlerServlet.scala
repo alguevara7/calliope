e0a9f06767d8c8426e9c905fdbc8a441284230f5
alexei guevara <alexguev@gmail.com>
2010-12-13 00:40:56 -0500

    print message upon email reception

--- src/main/scala/net/ushadow/calliope/web/MailHandlerServlet.scala
+++ src/main/scala/net/ushadow/calliope/web/MailHandlerServlet.scala
@@ -1,5 +1,7 @@
 package net.ushadow.calliope.web
 
+import org.slf4j.LoggerFactory
+import ch.qos.logback.classic.Logger
 import org.apache.commons.io.IOUtils
 import javax.mail.Multipart
 import javax.mail.internet.MimeMessage
@@ -10,17 +12,25 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServlet
 
 class MailHandlerServlet extends HttpServlet {
-
 	
+	private val logger = LoggerFactory.getLogger("MailHandlerServlet")
+
   override def doPost(req: HttpServletRequest, resp: HttpServletResponse) = {
-	  val props = new Properties
-      val session = Session.getDefaultInstance(props, null)
-      val message = new MimeMessage(session, req.getInputStream)
-      val content = message.getContent.asInstanceOf[Multipart]
-	  for (i <- 1 to content.getCount-1) {
-	 	  val part = content.getBodyPart(i)
-	 	  println(IOUtils.toString(part.getInputStream))
-	  }
-	   	
+    val props = new Properties
+    val session = Session.getDefaultInstance(props, null)
+    val message = new MimeMessage(session, req.getInputStream)
+
+    val content = message.getContent
+
+    if (content.isInstanceOf[Multipart]) {
+      val _content = content.asInstanceOf[Multipart]
+      for (i <- 1 to _content.getCount - 1) {
+        val part = _content.getBodyPart(i)
+        logger.info(IOUtils.toString(part.getInputStream))
+      }
+    } else {
+    	logger.info(content.asInstanceOf[String])
+    }
+
   }
 }
